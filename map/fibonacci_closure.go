ff31c5ae10157d879dd48b78c97ebfb817df6b94
Loi Nguyen <loinguyen@chotot.vn>
2016-12-01 20:56:51 +0700

    add fibonacci

---
+++ map/fibonacci_closure.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+// fibonacci is a function that returns
+// a function that returns an int.
+
+func fibonacci() func() int {
+	x := 0
+	y := 1
+	z := 0
+	return func() int {
+		z, y, x = x, x+y, y
+		return z
+	}
+}
+
+func main() {
+	f := fibonacci()
+	for i := 0; i < 10; i++ {
+		fmt.Println(f())
+	}
+}
