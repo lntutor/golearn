448d001fd158f391c94b47de824a2f0cbd2bce76
Loi Nguyen <loinguyen@chotot.vn>
2016-12-02 12:40:46 +0700

    interface

---
+++ chap3/interfaces.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Abser interface {
+	Abs() float64
+}
+
+type Vertex struct {
+	X, Y float64
+}
+
+type MyFloat float64
+
+func (f MyFloat) Abs() float64 {
+	if f < 0 {
+		return float64(-f)
+	}
+	return float64(f)
+}
+
+func (v *Vertex) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func main() {
+	var a Abser
+	vertex := Vertex{3, 4}
+	a = &vertex
+	fmt.Println(a.Abs())
+	myfloat := MyFloat(-3.2)
+	a = myfloat
+	fmt.Println(a.Abs())
+}
