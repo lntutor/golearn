0498483386e42ede4ce878c81b393b6a9810fc54
Loi Nguyen <loinguyen@chotot.vn>
2016-12-05 14:48:26 +0700

    rotate 3 reader

---
+++ chap3/rot13Reader.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"io"
+	"os"
+	"strings"
+)
+
+type rot13Reader struct {
+	r io.Reader
+}
+
+func rotate13(s byte) byte {
+	b := rune(s)
+	if (b >= 'a' && b <= 'm') || (b >= 'A' && b <= 'M') {
+		s += 13
+	} else {
+		s -= 13
+	}
+	return s
+}
+
+func (rot13 rot13Reader) Read(data []byte) (readed int, err error) {
+	readed, err = rot13.r.Read(data)
+	for i := 0; i < readed; i++ {
+		data[i] = rotate13(data[i])
+	}
+	return
+}
+
+func main() {
+	s := strings.NewReader("Lbh penpxrq gur pbqr!")
+	r := rot13Reader{s}
+	io.Copy(os.Stdout, &r)
+}
